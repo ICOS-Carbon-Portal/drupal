b960cf464522ce467bf392eb6a05107c0cc0e4dc
paul <paul.hedberg@nateko.lu.se>
2016-02-16 13:47:43 +0100

    Add archive to CP Events.

---
+++ modules/cp_events/src/Plugin/Block/ListOfArchivedCpEvents.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\cp_events\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\cp_events\CPEvents\SortedListOfEvents;
+use Drupal\Core\StreamWrapper\PublicStream;
+
+/**
+ * @Block(
+ *   id = "list_of_archived_cp_events",
+ *   admin_label = @Translation("List of archived CP events"),
+ * )
+ */
+class ListOfArchivedCpEvents extends BlockBase {
+
+	function build() {
+		\Drupal::service('page_cache_kill_switch')->trigger();
+		
+		$listOfEvents = new SortedListOfEvents();
+		$list = $listOfEvents->getListLatestFirst();
+		
+		$list_of_elements = array();
+		
+		foreach ($list as $e) {
+			if ($e->getHistorical() == 1) {
+				
+				$date = '';
+				if ($e->getFromDate() != null || $e->getFromDate() != '') {
+					$date = $e->getFromDate();
+				
+				} else {
+					$date = date('Y-m-d', $e->getChanged());
+					$e->setFromDate($date);
+				}
+				
+				$dateTime = new \DateTime($date);
+				$year = $dateTime->format('Y');
+				
+				$list_of_elements[$year][] = $e;
+				
+			}
+		}
+		
+		return array(
+				'#theme' => 'cp_events_archive',
+				'#elements' => $list_of_elements,
+				'#attached' => array(
+						'library' =>  array(
+								'cp_events/style',
+								'cp_events/script'
+						),
+				),
+		);
+	}
+}
