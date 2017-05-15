131136bb0e1b5170e21571b524bce6ccad8f60d7
Paul Hedberg <paul.hedberg@nateko.lu.se>
2017-05-15 13:03:08 +0200

    Add support for ACTRIS.

---
+++ themes/cp_theme_d8/js/cp_theme_d8-ac.js
@@ -0,0 +1,101 @@
+(function ($) {
+  'use strict';
+  Drupal.behaviors.cp_theme_d8 = {
+    attach: function(context, settings) {
+    	
+	    checkCpUserConsent();
+	    
+	    makeToplinksUnclickable();
+        
+    	$(function() {
+    		
+    		if ($(window).width() < 800) {
+    			fixSmallsizeMenu();		
+    		}
+    		
+    		$(window).resize(function () {
+    			if ($(window).width() < 800) {
+        			fixSmallsizeMenu();	
+        			
+        		}
+    		});
+    		
+    	});
+    }
+  };
+}(jQuery));
+
+function fixSmallsizeMenu() {
+	jQuery('#cp_theme_d8_menu .is_topnode').once('cp_theme_d8').each(
+			
+			function(index, value) {
+				jQuery(value).prepend('<img src="/themes/cp_theme_d8/images/arrow-up-down.svg" class="open_menu" title="Open/Close" />');
+				
+			}
+	);
+	
+	jQuery('#cp_theme_d8_menu .is_topnode .open_menu').once('cp_theme_d8').click(	    				
+		function(event) {
+			jQuery(this).parent().toggleClass('open');
+		}
+	);
+}
+
+function getCookieByName(cn) {
+    var n = cn + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") c = c.substring(1);
+        if (c.indexOf(n) == 0) { return c.substring(n.length, c.length); }
+    }
+    return "";
+}
+
+function checkCpUserConsent() {
+	if (checkedCpUserConsent) { return; }
+	
+    var hasConsent = getCookieByName("cpuserconsent");
+    
+    if (hasConsent === "") {
+        var info = document.createElement("p");
+        info.className = "info";
+        var infot = document.createTextNode("We use cookies to give you a better experience on our website. If you continue on our site you agree that we store cookies in your web browser.");
+        info.appendChild(infot);
+        var agree = document.createElement("p");
+        agree.className = "agree";
+        var agreet = document.createTextNode("I understand!");
+        var check = document.createElement("input");
+        check.type = "checkbox";
+        check.name = "iagree";
+        check.addEventListener("click", function() {
+			var d = new Date();
+			d.setTime(d.getTime() + (10*12*30*24*60*60*1000));
+			var expires = "expires=" + d.toUTCString();
+			document.cookie = "cpuserconsent=true;" + expires;
+			var div = document.getElementById("cpuserconsent");
+			div.parentNode.removeChild(div);	
+		});
+        agree.appendChild(agreet);
+        agree.appendChild(check);
+        var div = document.createElement("div");
+	    div.id = "cpuserconsent";
+        div.appendChild(info);
+        div.appendChild(agree);
+		var page = document.getElementById("page");
+		page.insertBefore(div, page.firstChild);    
+    }
+    checkedCpUserConsent = true;
+}
+
+checkedCpUserConsent = false;
+
+function makeToplinksUnclickable() {	
+	jQuery('#cp_theme_d8_menu_breadcrumbs').hide();
+
+	jQuery('#cp_theme_d8_menu a').each(function(i, e) {
+		if (jQuery(e).attr('href') === '/internal:#') {
+			jQuery(e).attr('href', 'javascript:void(0);');  //jQuery(location).attr('href')
+		}
+	});
+}
