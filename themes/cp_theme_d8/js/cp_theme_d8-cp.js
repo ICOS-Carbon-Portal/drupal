a47367512b25e56a9d87a9d4b73434a2f545fd0d
Jonathan Thiry <github@jonathanthiry.com>
2018-02-07 16:27:36 +0100

    Use correct js and css files when switching menu style

--- themes/cp_theme_d8/js/cp_theme_d8-cp.js
+++ themes/cp_theme_d8/js/cp_theme_d8-cp.js
@@ -25,22 +25,6 @@ jQuery(window).resize(function () {
 	}
 });
 
-function fixSmallsizeMenu() {
-	jQuery('#cp_theme_d8_menu .is_topnode').once('cp_theme_d8').each(
-
-			function(index, value) {
-				jQuery(value).prepend('<img src="/themes/cp_theme_d8/images/arrow-up-down.svg" class="open_menu" title="Open/Close" />');
-
-			}
-	);
-
-	jQuery('#cp_theme_d8_menu .is_topnode .open_menu').once('cp_theme_d8').click(
-		function(event) {
-			jQuery(this).parent().toggleClass('open');
-		}
-	);
-}
-
 /**
 * Home page
 * Tweets on home page.
