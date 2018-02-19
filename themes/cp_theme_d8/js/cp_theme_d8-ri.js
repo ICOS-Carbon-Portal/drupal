095df45772ed22f4e3d8519da38bc6c6092d7b6e
Jonathan Thiry <github@jonathanthiry.com>
2018-02-19 09:54:34 +0100

    Fix icos ri homepage

--- themes/cp_theme_d8/js/cp_theme_d8-ri.js
+++ themes/cp_theme_d8/js/cp_theme_d8-ri.js
@@ -17,57 +17,6 @@ function handleIngressAndTitle() {
 	jQuery('.field--name-field-cp-page-ingress .field__item').css({'padding':'0 0 3rem 0', 'font-size':'1.4rem', 'font-weight':'bold', 'text-transform':'uppercase', 'text-align':'center'});
 }
 
-/**
-* Home page
-* Tweets, Teasing blog and Station map on home page.
-* The following methods assume an embedded Tweet, a Teased CP Blog and a iframe (Google Map) in a respectively block on main content.
-* The methods also depends of a CP Event's at right sidebar.
-*/
-function fixHome() {
-	jQuery('#block-tweets').css({'background-color':'#f6f6f2', 'width':480, 'height':440});
-	jQuery('#block-tweets .content').css({'height':440, 'overflow-y':'scroll'});
-	jQuery('#block-viewteasedcpblog').css({'background-color':'#f6f6f2', 'height':440, 'border-left':'20px solid #f6f6f2', 'border-right':'20px solid #f6f6f2'});
-	jQuery('#block-viewteasedcpblog .teaser').css({'width':'100%'});
-	jQuery('#block-viewteasedcpblog .content h2').css({'padding-top':20});
-
-	jQuery('#block-stationmap').css({'border-top':'5px solid #f6f6f2', 'background-color':'#f6f6f2'});
-    jQuery('#block-stationmap h2').css({'margin-left':'2rem'});
-    jQuery('#block-stationmap > .content').css({'border-left':'20px solid #f6f6f2', 'border-right':'20px solid #f6f6f2'});
-    jQuery('#block-stationmap .to_bigger_map').css({'background-color':'#f6f6f2'});
-    jQuery('#block-stationmap .content .text-formatted').css({'height':0});
-    jQuery('#block-stationmap #map').css({'height':480});
-}
-
-function fixHomeMax() {
-	jQuery('#content').css({'min-height':'auto'});
-
-	jQuery('#block-listofteasedcpnews').css({'height':780});
-	jQuery('#block-listofteasedcpevents').css({'height':1045});
-
-	jQuery('#block-tweets').css({'position':'relative', 'top':5});
-
-	jQuery('#block-viewteasedcpblog').css({'position':'relative', 'bottom':455, 'left':500});
-	jQuery('#block-viewteasedcpblog').css({'width':444});
-
-    jQuery('#block-stationmap').css({'position':'relative', 'bottom':415, 'width':940, 'height':565});
-
-}
-
-function fixHomeMin() {
-	jQuery('#content').css({'min-height':2300});
-
-	jQuery('#block-listofteasedcpnews').css({'height':'auto'});
-	jQuery('#block-listofteasedcpevents').css({'height':'auto'});
-
-	jQuery('#block-tweets').css({'position':'relative', 'top':0});
-
-	jQuery('#block-viewteasedcpblog').css({'position':'relative', 'bottom':0, 'left':0});
-	jQuery('#block-viewteasedcpblog').css({'width':480});
-
-	jQuery('#block-stationmap').css({'position':'relative', 'bottom':-20, 'width':480, 'height':440});
-
-}
-
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 
