6dcc8df14a6d04c8fbf635081a425009f5cc98a2
Jonathan Thiry <github@jonathanthiry.com>
2019-05-10 14:17:17 +0200

    Update icos-ri styles

--- themes/cp_theme_d8/js/cp_theme_d8-ri.js
+++ themes/cp_theme_d8/js/cp_theme_d8-ri.js
@@ -7,27 +7,10 @@
   };
 }(jQuery));
 
-/**
- * This method assume the field cp_page_ingress:
- * Text (plain, long) Machine name: field_cp_page_ingress
- */
-function handleIngressAndTitle() {
-	jQuery('.page-title').css({'padding':'0 0 1rem 0', 'border-bottom':'0.1rem dashed #c7c8ca'});
-	jQuery('.field--name-field-cp-page-ingress .field__label').hide();
-	jQuery('.field--name-field-cp-page-ingress .field__item').css({'padding':'0 0 3rem 0', 'font-size':'1.4rem', 'font-weight':'bold', 'text-transform':'uppercase', 'text-align':'center'});
-}
-
-
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-
-
 ga('create', 'UA-53530911-2', 'auto');
-
 ga('send', 'pageview');
