790514bb28bb55e48899336887f48042cb2faf81
Jonathan Thiry <github@jonathanthiry.com>
2019-07-11 15:37:10 +0200

    Style header links

--- themes/cp_theme_d8/js/menu-login.js
+++ themes/cp_theme_d8/js/menu-login.js
@@ -1,30 +1,36 @@
 (function ($) {
-  'use strict';
-  $(document).ready(function () {
-    $.ajax({
-      url: 'https://cpauth.icos-cp.eu/whoami',
-      xhrFields: { withCredentials: true }
-  	}).done(function(response) {
-  		$("#logOutLnk").on('click', function() {
-  			$.ajax({
-          url: 'https://cpauth.icos-cp.eu/logout',
-          xhrFields: { withCredentials: true }
-        }).done(function(res){
-  				window.location.reload();
-  			});
-  		});
-  		$("#accountLnk").on('click', function() {
-  			window.location = 'https://cpauth.icos-cp.eu/';
-  		});
-  		$("#logOutLnk, #accountLnk").show();
-  	})
-    .fail(function(jqXHR) {
-      if (jqXHR.status == 401) {
-        $("#logInLnk").on('click', function() {
-          window.location = 'https://cpauth.icos-cp.eu/login/?targetUrl=' + encodeURIComponent(window.location.href);
-        });
-        $("#logInLnk").show();
-      }
-    });
-  });
+	'use strict';
+	$(document).ready(function () {
+		$.ajax({
+			url: 'https://cpauth.icos-cp.eu/whoami',
+			xhrFields: { withCredentials: true },
+			error: showLoginLink
+		})
+			.done(function() {
+				$(".logOutLnk").on('click', function() {
+					$.ajax({
+						url: 'https://cpauth.icos-cp.eu/logout',
+						xhrFields: { withCredentials: true }
+					}).done(function(){
+						window.location.reload();
+					});
+				});
+				$(".accountLnk").on('click', function() {
+					window.location = 'https://cpauth.icos-cp.eu/';
+				});
+				$(".logOutLnk, .accountLnk").show();
+			})
+			.fail(function(jqXHR) {
+				if (jqXHR.status == 401) {
+					showLoginLink();
+				}
+			});
+	});
+
+	function showLoginLink() {
+		$(".logInLnk").on('click', function() {
+			window.location = 'https://cpauth.icos-cp.eu/login/?targetUrl=' + encodeURIComponent(window.location.href);
+		});
+		$(".logInLnk").show();
+	}
 }(jQuery));
