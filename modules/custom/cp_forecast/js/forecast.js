7561cbc6ad181a0045c9a08dedeceaa2d0234673
Jonathan Thiry <github@jonathanthiry.com>
2020-05-07 11:33:33 +0200

    [cp_forecast] Add error message
    Display an error message when videos are not available.

--- modules/custom/cp_forecast/js/forecast.js
+++ modules/custom/cp_forecast/js/forecast.js
@@ -12,15 +12,29 @@
     $.ajax({
       url: `${apiBaseUrl}/mpg/`,
     }).done(function(files) {
-      const fileOptions = files.map((file) => {
-        return `<option value="${file.name}">${file.name.replace(/\.[^/.]+$/, "")}</option>`;
-      });
-      $('#forecast-select').html(fileOptions);
-      $('#forecast-select').change(function() {
-        $('#video-player').attr('src', `${apiBaseUrl}/mpg/${$(this).val()}`);
-        $('#video-download-button').attr('href', `${apiBaseUrl}/mpg/${$(this).val()}`);
-        $('#data-download-button').attr('href', `${apiBaseUrl}/trj/TI_${$(this).val().replace(/\.[^/.]+$/, ".gz")}`);
-      }).trigger('change');
+      if (files.length > 0) {
+        const fileOptions = files.map((file) => {
+          return `<option value="${file.name}">${file.name.replace(/\.[^/.]+$/, "")}</option>`;
+        });
+        $('#forecast-select').html(fileOptions);
+        $('#forecast-select').change(function() {
+          $('#video-player').attr('src', `${apiBaseUrl}/mpg/${$(this).val()}`);
+          $('#video-download-button').attr('href', `${apiBaseUrl}/mpg/${$(this).val()}`);
+          $('#data-download-button').attr('href', `${apiBaseUrl}/trj/TI_${$(this).val().replace(/\.[^/.]+$/, ".gz")}`);
+        }).trigger('change');
+      } else {
+        showErrorMessage();
+      }
+    }).fail(function() {
+      showErrorMessage();
     });
+  }
+
+  function showErrorMessage() {
+    $('#alert-placeholder').html(
+      `<div class="alert alert-danger" role="alert">
+        Forecasts are not available right now. Please try again soon!
+      </div>`);
+    $('.forecast-controls').hide();
   }
 })(jQuery, Drupal);
