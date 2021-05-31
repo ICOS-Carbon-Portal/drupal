4314e0da84c9012985c08bfdeab58b4a9efea3e7
Roger Groth <roger.groth@gis.lu.se>
2021-05-31 15:01:29 +0200

    [cp_station_list] Use common code on statis for fetching and parsing stations.

--- modules/custom/cp_station_list/js/station-list.js
+++ modules/custom/cp_station_list/js/station-list.js
@@ -1,137 +1,5 @@
 (function ($, Drupal) {
 
-	const config = {
-		sparqlUrl: "https://meta.icos-cp.eu/sparql",
-		stationVisUrl: "https://meta.icos-cp.eu/station/"
-	};
-
-	const getIconUrl = themeShort =>
-		`https://static.icos-cp.eu/share/stations/icons/${themeShort.toLowerCase()}.png`;
-
-	const Vars = {
-		lat: 'latstr',
-		lon: 'lonstr',
-		geoJson: 'geoJson',
-		prodUri: 'prodUri',
-		stationId: 'Id',
-		stationName: 'Name',
-		country: 'Country',
-		theme: 'Theme',
-		themeShort: 'themeShort',
-		pi: 'PI_names',
-		siteType: 'Site_type',
-		seaElev: 'Elevation_above_sea',
-		groundElev: 'Elevation_above_ground',
-		stationClass: 'Station_class',
-		labelingDate: 'Labeling_date',
-		coords: 'Location'
-	};
-
-	const Columns = [
-		Vars.stationId, Vars.stationName, Vars.theme, Vars.stationClass, Vars.coords, Vars.country,
-		Vars.pi, Vars.siteType, Vars.seaElev, Vars.labelingDate, Vars.lat, Vars.lon, Vars.geoJson, Vars.themeShort
-	];
-
-	function idx(varName){
-		return Columns.indexOf(varName);
-	}
-
-	const provQuery = `PREFIX cpst: <http://meta.icos-cp.eu/ontologies/stationentry/>
-SELECT *
-FROM <http://meta.icos-cp.eu/resources/stationentry/>
-WHERE {
-	{
-		select ?s (GROUP_CONCAT(?piLname; separator=";") AS ?${Vars.pi})
-		where{ ?s cpst:hasPi/cpst:hasLastName ?piLname }
-		group by ?s
-	}
-	?s a ?owlClass .
-	BIND(REPLACE(str(?owlClass),"http://meta.icos-cp.eu/ontologies/stationentry/", "") AS ?${Vars.themeShort})
-	?s cpst:hasShortName ?${Vars.stationId} .
-	?s cpst:hasLongName ?${Vars.stationName} .
-	OPTIONAL{?s cpst:hasLat ?${Vars.lat} . ?s cpst:hasLon ?${Vars.lon} }
-	OPTIONAL{?s cpst:hasSpatialReference ?${Vars.geoJson} }
-	OPTIONAL{?s cpst:hasCountry ?${Vars.country} }
-	OPTIONAL{?s cpst:hasSiteType ?${Vars.siteType} }
-	OPTIONAL{?s cpst:hasElevationAboveSea ?${Vars.seaElev} }
-	#OPTIONAL{?s cpst:hasElevationAboveGround ?${Vars.groundElev} }
-	OPTIONAL{?s cpst:hasStationClass ?${Vars.stationClass} }
-}`;
-
-const prodQuery = `prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
-prefix cpst: <http://meta.icos-cp.eu/ontologies/stationentry/>
-select *
-from <http://meta.icos-cp.eu/resources/icos/>
-from <http://meta.icos-cp.eu/resources/cpmeta/>
-from <http://meta.icos-cp.eu/resources/stationentry/>
-where{
-	{
-		select ?s ?ps (GROUP_CONCAT(?lname; separator=";") AS ?${Vars.pi}) where {
-			?s cpst:hasProductionCounterpart ?psStr .
-			bind(iri(?psStr) as ?ps)
-			?memb cpmeta:atOrganization ?ps ; cpmeta:hasRole <http://meta.icos-cp.eu/resources/roles/PI> .
-			filter not exists {?memb cpmeta:hasEndTime []}
-			?pers cpmeta:hasMembership ?memb ; cpmeta:hasLastName ?lname .
-		}
-		group by ?s ?ps
-	}
-	?ps cpmeta:hasStationId ?${Vars.stationId} ; cpmeta:hasName ?${Vars.stationName} .
-	optional{ ?ps cpmeta:hasElevation ?${Vars.seaElev} }
-	optional{ ?ps cpmeta:hasLatitude ?${Vars.lat}}
-	optional{ ?ps cpmeta:hasLongitude ?${Vars.lon}}
-	optional{ ?ps cpmeta:hasSpatialCoverage/cpmeta:asGeoJSON ?${Vars.geoJson}}
-	optional{ ?ps cpmeta:countryCode ?${Vars.country}}
-	optional{ ?ps cpmeta:hasStationClass  ?${Vars.stationClass}}
-	optional{ ?ps cpmeta:hasLabelingDate ?${Vars.labelingDate}}
-	bind(?ps as ?${Vars.prodUri})
-}
-`;
-
-	const fetchStations = query => $.ajax({
-		type: "POST",
-		data: {query},
-		url: config.sparqlUrl,
-		dataType: "json"
-	});
-
-
-	function parseStationsJson(bindings){
-		const themeName = {"AS": "Atmosphere", "ES": "Ecosystem", "OS": "Ocean"};
-
-		const modifiers = {
-			[Vars.theme]:       (v, row) => themeName[row[Vars.themeShort].value] || "?",
-			[Vars.country]:      v => `${countries[v]} (${v})`,
-			[Vars.pi]:           v => v.split(';').sort().join("<br>"),
-			[Vars.stationClass]: v => (v == "Ass" ? "Associated" : v),
-			[Vars.siteType]:     v => v.toLowerCase(),
-			[Vars.stationId]:   (v, row) => (row[Vars.prodUri]
-				? `<a target="_blank" href="${row[Vars.prodUri].value}">${v}</a>`
-				: v
-			)
-		}
-
-		var rows = bindings.map(row =>
-			Columns.map(col => {
-				const v = (row[col] || {}).value || "";
-				const modifier = modifiers[col];
-				return modifier ? modifier(v, row) : v;
-			})
-		);
-
-		return ({
-			rows,
-			columns: Columns.map(col => ({title: col.replace(/_/g, " ")}))
-		});
-	}
-
-	function mergeProvAndProd(prov, prod){
-		const prodLookup = prod[0].results.bindings.reduce((acc, next) => {
-			acc[next.s.value] = next;
-			return acc;
-		}, {});
-		return prov[0].results.bindings.map(row => $.extend({}, row, prodLookup[row.s.value]));
-	}
-
 	function showMap(title, theme, lat, lon, geoJson) {
 		if(geoJson == null && (lat == null || lon == null)) return;
 
@@ -139,42 +7,44 @@ where{
 		const coverageJson = geoJson || `{"coordinates":[${lon}, ${lat}],"type":"Point"}`;
 		const coverageQ = 'coverage=' + encodeURIComponent(coverageJson);
 
-		const $frame = $(`<iframe style="border:0;width:100%;height:100%" src="${config.stationVisUrl}?${coverageQ}${iconQ}"></iframe>`);
+		const $frame = $(`<iframe style="border:0;width:100%;height:100%" src="${urls.stationVisUrl}?${coverageQ}${iconQ}"></iframe>`);
 
 		$("#stationMapModalLabel").text(title);
 		$('#stationMapModalBody').html($frame);
 		$("#station-map").modal();
 	}
 
-	function initDataTable(stations){
-		$('#stationsTable').DataTable( {
+	function initDataTable(stations) {
+		$('#stationsTable').DataTable({
 			data: stations.rows,
 			columns: stations.columns,
 			columnDefs: [
 				{
 					//Hide some columns
-					targets: [Vars.lat, Vars.lon, Vars.geoJson, Vars.themeShort].map(idx),
+					targets: [Vars.lat, Vars.lon, Vars.geoJson, Vars.themeShort].map(varNameIdx),
 					visible: false,
 					searchable: false
 				},
 				{
-					targets: [idx(Vars.coords)],
+					targets: [varNameIdx(Vars.coords)],
 					fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
 
 						const [stLat, stLon, stName, stTheme, stGeojson] = [
 							Vars.lat, Vars.lon, Vars.stationName, Vars.themeShort, Vars.geoJson
-						].map(vname => oData[idx(vname)]);
+						].map(vname => oData[varNameIdx(vname)]);
 
 						if (stLat == "" || stLon == "") {
 							if (stGeojson != "") {
-								var $icon = $('<i class="fas fa-map-marked-alt"></i>');
+								var $icon = $(`<span class="station-coordinates">
+								<i class="fas fa-map-marked-alt"></i>
+							</span>`);
 
 								$icon.click(() => showMap(stName, stTheme, null, null, stGeojson));
 
 								$(nTd).html($icon);
 							}
 						} else {
-							var $icon = $(`<span class="station-coordinates" data-target="#exampleModal">
+							var $icon = $(`<span class="station-coordinates">
 								<i class="fas fa-map-marked-alt"></i> (${stLat}, ${stLon})
 							</span>`);
 
@@ -214,11 +84,9 @@ where{
 		attach: function (context) {
 			$('body', context).once('stationListBehavior').each(function () {
 
-				$.when(fetchStations(provQuery), fetchStations(prodQuery))
-					.then(mergeProvAndProd)
-					.then(parseStationsJson)
-					.done(initDataTable)
-					.fail(function(err){
+				$.when(fetchMergeParseStations(new StationParser(countries)))
+					.then(initDataTable)
+					.fail(function (err) {
 						console.log(err);
 					});
 			});
