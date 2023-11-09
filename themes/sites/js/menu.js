99907961f27a2ea7a5bf3e6c41387f7354fa5b8a
Jonathan Thiry <github@jonathanthiry.com>
2023-11-09 10:52:15 +0100

    [sites] Update menu layout

--- themes/sites/js/menu.js
+++
@@ -1,8 +0,0 @@
-
-jQuery('#block-sites-main-menu .primary-nav-menu-item-has-children').once('sites').click(
-	function() {
-		if (window.innerWidth < 576) {
-			jQuery(this).toggleClass('open');
-		}
-	}
-);
