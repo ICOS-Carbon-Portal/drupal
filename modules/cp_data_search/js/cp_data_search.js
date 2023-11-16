bd8f271c24048ce2a98a2b4d448d8d1e8704adf5
Jonathan Thiry <github@jonathanthiry.com>
2023-11-16 13:54:20 +0100

    [cp_data_search] Update for Drupal 10

--- modules/cp_data_search/js/cp_data_search.js
+++ modules/cp_data_search/js/cp_data_search.js
@@ -1,37 +1,60 @@
-(function($, Drupal) {
-	Drupal.behaviors.dataSearchBehavior = {
-		attach: function(context, settings) {
-			$('.type .box-content', context).once('dataSearchBehavior').each(function() {
-				$(this).click(function() {
-					$('.type input').not($(this).siblings('input')).prop('checked', false);
-				});
-			});
+Drupal.behaviors.dataSearchBehavior = {
+	attach: function(context) {
+		const form = document.getElementById('js-data-search-form');
+		form.querySelectorAll('.box-input').forEach((input) => {
+			input.checked = false;
+		});
 
-			$('.type .box-input', context).once('dataSearchBehavior').each(function() {
-				$(this).change(function() {
-					if ($('.type input:checked').val() == '1,2') {
-						$('.themes').show();
-					} else {
-						$('.themes').hide();
-						$('.theme input').prop('checked', false);
-					}
+		once('dataSearchBehavior', '.type .box-input', context).forEach((el) => {
+			el.addEventListener('change', function () {
+				Array.from(form.querySelectorAll('.type input')).filter((input) => input != el).forEach((el) => {
+					el.checked = false;
 				});
-			});
 
-			$('#js-data-search-form', context).once('dataSearchBehavior').each(function() {
-				$('.box-input').prop('checked', false);
-				$(this).submit(function() {
-					const level = $('input[name=type]:checked').val();
-					const levelQuery = typeof level !== 'undefined' ? `"level":[${$('input[name=type]:checked').val()}]` : '';
-					const themes = $('input[name=theme]:checked').map(function(){return this.value;}).get();
-					const themeQuery = level == '1,2' && themes.length ? `,"theme":["${themes.join('","')}"]` : '';
-					const project = level == '1,2' ? `,"project":["icos"]` : '';
-					const query = typeof level !== 'undefined' ? `#{"filterCategories":{${levelQuery}${themeQuery}${project}}}` : `#{"filterCategories":{"project":["icos"]}}`;
+				const levels = getSelectedLevels()
 
-					$(location).attr('href', `https://data.icos-cp.eu/portal/${query}`);
-					return false;
-				});
+				if (levels == '1,2') {
+					form.querySelector('.themes').style.display = 'block';
+				} else {
+					form.querySelector('.themes').style.display = 'none';
+					form.querySelectorAll('.theme input').forEach((input) => {
+						input.checked = false;
+					});
+				}
+
+				updateButtonLink(levels);
 			});
+		});
+
+		once('dataSearchBehavior', '.theme .box-input', context).forEach((el) => {
+			el.addEventListener('change', function () {
+				const levels = getSelectedLevels()
+				const themes = getSelectedThemes();
+
+				updateButtonLink(levels, themes);
+			});
+		});
+
+		function getSelectedLevels() {
+			let checkedTypes = form.querySelector('input[name=type]:checked');
+			return checkedTypes && checkedTypes.value;
 		}
-	};
-})(jQuery, Drupal);
+
+		function getSelectedThemes() {
+			return Array.from(form.querySelectorAll('input[name=theme]:checked'), ({ value }) => value);
+		}
+
+		function updateButtonLink(levels, themes) {
+			const levelQuery = typeof levels !== 'undefined' ? `"level":[${levels}]` : '';
+			const themeQuery = levels == '1,2' && themes.length ? `,"theme":["${themes.join('","')}"]` : '';
+			const project = levels == '1,2' ? `,"project":["icos"]` : '';
+
+			const query = typeof levels !== 'undefined'
+				? `#{"filterCategories":{${levelQuery}${themeQuery}${project}}}`
+				: `#{"filterCategories":{"project":["icos"]}}`;
+
+			form.querySelector('.btn-find-data').href = `https://data.icos-cp.eu/portal/${query}`;
+		}
+
+	}
+};
