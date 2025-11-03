2d84c6a018d68fed3b13b510f1341d763791c0fa
Andrew Debevec <andrew.debevec@nateko.lu.se>
2025-11-03 11:57:54 +0100

    WIP Add new theme

---
+++ themes/cp_theme_d10/js/global.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+  'use strict';
+
+  Drupal.behaviors.cp_theme_d10 = {
+    attach: function (context, settings) {},
+  };
+})(Drupal);
