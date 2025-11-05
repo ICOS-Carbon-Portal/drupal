a60fbf1a0f5c292415cbc44ed46d2aa29f709463
Andrew Debevec <andrew.debevec@nateko.lu.se>
2025-11-05 11:35:27 +0100

    Add JS to keep collapsible filter state consistent

---
+++ modules/custom/cp_services/js/cp-services-view.js
@@ -0,0 +1,38 @@
+(function (Drupal, drupalSettings) {
+    function makeQuery(id) {
+        return '[data-drupal-selector="' + id + '"]';
+    }
+
+    function isFilterOpen(id) {
+        return document.querySelector(makeQuery(id)).open;
+    }
+    
+    let filters = [
+        {id: "edit-access-rights-value-collapsible", open: isFilterOpen("edit-access-rights-value-collapsible")},
+        {id: "edit-group-value-collapsible", open: isFilterOpen("edit-group-value-collapsible")},
+        {id: "edit-main-users-value-collapsible", open: isFilterOpen("edit-main-users-value-collapsible")}
+    ];
+
+    // addEventListener for click on elements with ids = id within context
+    function toggleFilter(e) {
+        console.log("filters before");
+        console.log(filters);
+        console.log(e.target);
+        filters = filters.map((filter) => filter.id === e.target.dataset["drupalSelector"] ? {id: filter.id, open: e.target.open} : filter);
+        console.log("filters after");
+        console.log(filters);
+    }
+
+    Drupal.behaviors.servicesViewRefresh = {
+        attach: function(context, settings) {
+            for(let filter of filters) {
+                // open filters that were previously open
+                if (filter.open) {
+                    context.querySelector(makeQuery(filter.id)).open = true;
+                }
+                // listen for "toggle" event, maintain external state
+                context.querySelector(makeQuery(filter.id)).addEventListener("toggle", toggleFilter);
+            }
+        }
+    }
+})(Drupal, drupalSettings);
