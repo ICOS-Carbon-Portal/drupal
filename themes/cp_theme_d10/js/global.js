8594bdbd9ce5a2c985fdc72034fe213aca1f7449
Andrew Debevec <andrew.debevec@nateko.lu.se>
2025-11-27 16:36:41 +0100

    WIP Restyling and templating menu for redesign

--- themes/cp_theme_d10/js/global.js
+++ themes/cp_theme_d10/js/global.js
@@ -7,6 +7,20 @@
   'use strict';
 
   Drupal.behaviors.cp_theme_d10 = {
-    attach: function (context, settings) {},
+    attach: function (context, settings) {
+      /*once('cp_theme_d10', '#menu-button', context).forEach(function(button) {
+        button.addEventListener('click', function() {
+          document.getElementById('cp_theme_d10_menu').classList.toggle('open');
+        });
+      });*/
+
+      once('cp_theme_d10', '#cp_theme_d10_menu .top-node .open-menu').forEach(function (el) {
+        el.addEventListener('click', function () {
+          el.parentElement.parentElement.classList.toggle('open');
+        });
+        console.log("added listener to ");
+        console.log(el);
+      });
+    },
   };
 })(Drupal);
