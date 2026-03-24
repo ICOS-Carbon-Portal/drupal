6a81eb8b3cc87557c8f5ee4cce7f9bdf342111e8
Andrew Debevec <andrew.debevec@nateko.lu.se>
2026-03-24 13:48:49 +0100

    Reformat CSS, remove comments, add EOF return

--- themes/cp_theme_d10/js/global.js
+++ themes/cp_theme_d10/js/global.js
@@ -8,12 +8,6 @@
 
   Drupal.behaviors.cp_theme_d10 = {
     attach: function (context, settings) {
-      /*once('cp_theme_d10', '#menu-button', context).forEach(function(button) {
-        button.addEventListener('click', function() {
-          document.getElementById('cp_theme_d10_menu').classList.toggle('open');
-        });
-      });*/
-
       once('cp_theme_d10', '#cp_theme_d10_menu .top-node .dd-toggle').forEach(function (el) {
         el.addEventListener('click', function (event) {
           let target = el.closest(".top-node");
