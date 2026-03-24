34339ef148e23337724360784c9a902404ce01a8
Andrew Debevec <andrew.debevec@nateko.lu.se>
2026-03-24 13:16:46 +0100

    Factor out layout functions to parent

--- modules/custom/cp_layouts/src/Plugin/Layout/LightBlueThreeColumnLayout.php
+++ modules/custom/cp_layouts/src/Plugin/Layout/LightBlueThreeColumnLayout.php
@@ -2,43 +2,7 @@
 
 namespace Drupal\cp_layouts\Plugin\Layout;
 
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Layout\LayoutDefault;
-
 /**
  * Layout class for the Light Blue three-column layout.
  */
-class LightBlueThreeColumnLayout extends LayoutDefault {
-
-  public function defaultConfiguration() {
-    return parent::defaultConfiguration() + [
-      'spacing_top' => 5,
-      'spacing_bottom' => 5,
-    ];
-  }
-
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildConfigurationForm($form, $form_state);
-    $options = array_combine(range(0, 6), range(0, 6));
-    $form['spacing_top'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Top spacing'),
-      '#options' => $options,
-      '#default_value' => $this->configuration['spacing_top'],
-    ];
-    $form['spacing_bottom'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Bottom spacing'),
-      '#options' => $options,
-      '#default_value' => $this->configuration['spacing_bottom'],
-    ];
-    return $form;
-  }
-
-  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    parent::submitConfigurationForm($form, $form_state);
-    $this->configuration['spacing_top'] = $form_state->getValue('spacing_top');
-    $this->configuration['spacing_bottom'] = $form_state->getValue('spacing_bottom');
-  }
-
-}
+class LightBlueThreeColumnLayout extends ConfigurableSpacingLayout {}
