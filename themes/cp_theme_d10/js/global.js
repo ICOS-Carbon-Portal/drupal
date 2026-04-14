324fabc967148c0b7bdc12709ca769cb669ebe56
Andrew Debevec <andrew.debevec@nateko.lu.se>
2026-04-14 09:56:12 +0200

    Update global styling and naming in theme to facilitate views-core implementation (#23)
    1. In main menu, rename `.dd-toggle` to `.drop-down-toggle`
    2. In main menu, remove `.was-ul` class (artifact)
    3. Remove underline from footer links
    4. Update search button style to match navbar-toggler
    5. Update Bootstrap to point to 5.3.8 (which is deployed based on the
    changes in https://github.com/ICOS-Carbon-Portal/static/pull/20 )
    6. Update header padding to align logo with left content on small/medium
    screens
    7. Adjust search button sizing for consistency in mobile view
    8. Fix double border on header/nav menu in mobile view
    9. Fix padding on data products page

--- themes/cp_theme_d10/js/global.js
+++ themes/cp_theme_d10/js/global.js
@@ -8,7 +8,7 @@
 
   Drupal.behaviors.cp_theme_d10 = {
     attach: function (context, settings) {
-      once('cp_theme_d10', '#cp_theme_d10_menu .top-node .dd-toggle').forEach(function (el) {
+      once('cp_theme_d10', '#cp_theme_d10_menu .top-node .drop-down-toggle').forEach(function (el) {
         el.addEventListener('click', function (event) {
           let target = el.closest(".top-node");
           target.classList.toggle("open");
@@ -19,7 +19,7 @@
             let target = el.closest(".top-node");
             target.classList.toggle("open");
             event.preventDefault();
-            }
+          }
         });
       });
       once('cp_theme_d10', '#cp_theme_d10_menu').forEach(function (el) {
