bd6026e9dfe7e2a0e39538aa92141439bd3da9dc
Andrew Debevec <andrew.debevec@nateko.lu.se>
2026-05-11 17:05:15 +0200

    WIP Working sync between filters and buttons

--- modules/custom/cp_community_hub/js/cp-community-hub-view.js
+++ modules/custom/cp_community_hub/js/cp-community-hub-view.js
@@ -6,26 +6,28 @@
     const filterGroupsSelector = '[data-drupal-selector="edit-user-groups-target-id"]';
     const viewsFormId = 'views-exposed-form-cpcommhub-resources-block-1';
 
-    function moveUserGroupsFilter() {
+    function cloneUserGroupsFilter() {
         const groupContainer = document.querySelector(groupContainerSelector);
-        const filterWrapper = document.querySelector(filterGroupsSelector)?.closest('.form-item, .js-form-item, fieldset, details');
+        const filterEl = document.querySelector(filterGroupsSelector);
+        const filterWrapper = filterEl?.closest('.form-item, .js-form-item, fieldset, details');
 
         if (!groupContainer || !filterWrapper) {
             return;
         }
 
-        for (let checkbox of filterWrapper.querySelectorAll('input[type="checkbox"]')) {
+        const clone = filterWrapper.cloneNode(true);
+        for (let checkbox of clone.querySelectorAll('input[type="checkbox"]')) {
             checkbox.setAttribute('form', viewsFormId);
             checkbox.classList.add('btn-check');
             checkbox.setAttribute('autocomplete', 'off');
-            checkbox.closest('.form-check')?.classList.add('d-inline');
-            const label = filterWrapper.querySelector('label[for="' + checkbox.id + '"]');
+            const label = clone.querySelector('label[for="' + checkbox.id + '"]');
             if (label) {
                 label.classList.add('btn', 'btn-outline-primary');
             }
         }
 
-        groupContainer.appendChild(filterWrapper);
+        groupContainer.appendChild(clone);
+        filterWrapper.classList.add('d-none');
     }
 
     function hideGroupsFilterInView(context) {
@@ -58,7 +60,7 @@
 
     Drupal.behaviors.communityHubView = {
         attach: function (context, settings) {
-            once('commhub-move-groups', 'body', document).forEach(() => moveUserGroupsFilter());
+            once('commhub-clone-groups', 'body', document).forEach(() => cloneUserGroupsFilter());
 
             hideGroupsFilterInView(context);
 
@@ -68,6 +70,14 @@
                 container.addEventListener('change', (e) => {
                     if (e.target.matches('input[type="checkbox"]')) {
                         updateBlockVisibility();
+                        const form = document.getElementById(viewsFormId);
+                        const internalCb = form
+                            ? Array.from(form.querySelectorAll('input[type="checkbox"]')).find(cb => cb.name === e.target.name)
+                            : null;
+                        if (internalCb) {
+                            internalCb.checked = e.target.checked;
+                            internalCb.dispatchEvent(new Event('change', { bubbles: true }));
+                        }
                     }
                 });
             });
