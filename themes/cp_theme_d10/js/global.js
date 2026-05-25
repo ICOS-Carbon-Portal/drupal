0d1cc7c7431ea1ffcce36b432a3a1dc5a8324f8a
Andrew Debevec <andrew.debevec@nateko.lu.se>
2026-05-25 09:48:49 +0200

    WIP Basic working backdrop

--- themes/cp_theme_d10/js/global.js
+++ themes/cp_theme_d10/js/global.js
@@ -26,6 +26,38 @@
           }
         });
       });
+      const menuBackdrop = document.querySelector(".menu-backdrop");
+      function whenEntered(topNode) {
+        let height = topNode.dataset.computedHeight;
+        if (!height) {
+          const dropdown = topNode.querySelector(':scope > .dropdown-menu');
+          height = (dropdown ? dropdown.offsetHeight : topNode.offsetHeight) + 'px';
+          topNode.dataset.computedHeight = height;
+        }
+        menuBackdrop.style.height = height;
+      }
+
+      function whenExited(topNode) { // I might not need topNode here, but leave for now
+        menuBackdrop.style.height = "0px";
+      }
+      once('cp_theme_d10_hover', '#cp_theme_d10_menu .top-node').forEach(function (el) {
+
+        el.addEventListener('mouseover', function (event) {
+          whenEntered(el);
+        });
+        el.addEventListener('focusin', function (event) {
+          whenEntered(el);
+        });
+
+        el.addEventListener('mouseleave', function (event) {
+          whenExited(el);
+        });
+        el.addEventListener('focusout', function (event) {
+          if (!el.contains(event.relatedTarget)) {
+            whenExited(el);
+          }
+        });
+      });
       once('cp_theme_d10', '#cp_theme_d10_menu').forEach(function (el) {
         el.addEventListener('mouseout', function (event) {
           if (event.target === document.activeElement) {
